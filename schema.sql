d0181b50b532929d4adad0a1b4b537c5990e446b
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-13 19:56:19 -0600

    tables for department, roles, and employees added

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+CREATE TABLE department (
+id INTEGER AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INTEGER AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INTEGER NOT NULL,
+PRIMARY KEY (id),
+CONSTRAINT foreignKey_1 FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+id INTEGER AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER,
+manager_id INTEGER, 
+PRIMARY KEY (id),
+CONSTRAINT foreignKey_2 FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
+);
