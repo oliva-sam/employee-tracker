5cf3673785676903c43cde2ccd1ade11c5fd45a2
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-19 18:24:37 -0600

    added remaining prompts to user and began the paired callback function to the user answers

--- start.js
+++ start.js
@@ -1,8 +1,12 @@
+// Dependencies
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 require("console.table");
+//const util = require("util");
 
+// Setting up mySQL connection
 var connection = mysql.createConnection({
+
     host: "localhost",
 
     port: 3306,
@@ -13,10 +17,15 @@ var connection = mysql.createConnection({
     database: "employeeTrackerDB"
 });
 
+//let departmentIDs = []
+
 connection.connect(function (err) {
     if (err) throw err;
+    departmentIDs = currentDepartments();
     startQuestion();
 });
+
+//connection.query = util.promisify(connection.query);
 
 function startQuestion() {
     inquirer
@@ -26,60 +35,128 @@ function startQuestion() {
                 type: "list",
                 message: "What would you like to do?",
                 choices: [
-                    "View Employees",
-                    "View Departments",
-                    "View Roles",
+                    "View All Employees",
+                    "View All Departments",
+                    "View All Roles",
                     "Add Department",
-                    "Add Roles",
-                    "Add Employees",
+                    "Add Role",
+                    "Add Employee",
+                    "Update Employee Role",
                     "Exit Application"
                 ]
             }
         ])
         .then(function (response) {
-            switch(response.userToDo) {
-                case "View Employee":
-                    viewEmployee();
+            switch (response.userToDo) {
+                case "View All Employees":
+                    viewEmployees();
                     break;
-                case "View Department":
-                    viewDepartment();
+                case "View All Departments":
+                    viewDepartments();
+                    break;
+                case "View All Roles":
+                    viewRoles();
                     break;
                 case "Add Department":
                     addDepartment();
                     break;
-                    //NEED TO ADD ALL OF THE OPTIONS
-                default :
+                case "Add Role":
+                    addRole(departmentIDs);
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    addDepartment();
+                    break;
+                default:
                     connection.end();
                     process.exit(0);
             }
         })
 }
 
-function viewEmployee() {
-    var query =  "SELECT * FROM employee;";
-    connection.query (query, function(err, response) {
+function viewEmployees() {
+    var query = "SELECT * FROM employee;";
+    connection.query(query, function (err, data) {
         if (err) throw err;
-        console.table(response)
+        console.table(data)
+        startQuestion();
+    })
+}
+
+function viewDepartments() {
+    var query = "SELECT * FROM department;";
+    connection.query(query, function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        startQuestion();
+    })
+}
+
+function viewRoles() {
+    var query = "SELECT * FROM role;";
+    connection.query(query, function (err, data) {
+        if (err) throw err;
+        console.table(data)
         startQuestion();
     })
 }
 
 function addDepartment() {
-   inquirer
-   .prompt([
-       {
-           name: "new_department",
-           type: "input",
-           message: "What is the name of the department you would like to add?"
-       }
-   ])
-   .then(function(response) {
-     var query = "INSERT INTO department (name) values (?)";
-     connection.query (query, response.new_department, function(err, response) {
-        if (err) throw err;
-        console.table(response)
-        startQuestion();
-    })
-   })
+    inquirer
+        .prompt([
+            {
+                name: "new_department",
+                type: "input",
+                message: "What is the name of the department you would like to add?"
+            }
+        ])
+        .then(function (response) {
+            var query = "INSERT INTO department (name) values (?)";
+            connection.query(query, response.new_department, function (err, data) {
+                if (err) throw err;
+                console.table(data)
+                startQuestion();
+            })
+        })
 
 }
+
+function addRole(departmentIDs) {
+    console.log(departmentIDs)
+    inquirer
+        .prompt([
+            {
+                name: "new_role",
+                type: "input",
+                message: "What is the title of the role you would like to add?"
+            },
+            {
+                name: "new_role_salary",
+                type: "input",
+                message: "What is the salary for this new role?"
+            },
+            {
+                name: "new_role_department_id",
+                type: "input",
+                message: "What is the department id for this new role?",
+                // choices: currentDepartments()
+            }
+        ])
+        .then(function (response) {
+            var query = "INSERT INTO role (title, salary, department_id) values (?, ?, ?)";
+            connection.query(query, response.new_role, response.new_role_salary, function (err, data) {
+                if (err) throw err;
+                console.table(data)
+                startQuestion();
+            })
+        })
+
+}
+
+
+// function currentDepartments() {
+//     var query = "SELECT (id) FROM department";
+//     return connection.query(query);
+// }
